3a99692fc781607e88ddca9bf2f15f5d54b1d859
Patrick Gu <patrick@patrickgu.ca>
2024-01-26 22:23:14 -0500

    Program type declarations

---
+++ runtime/program.ts
@@ -0,0 +1,88 @@
+interface LiteralString {
+  type: "literalString";
+  value: string;
+}
+
+interface LiteralInteger {
+  type: "literalInteger";
+  value: number;
+}
+
+interface LiteralBoolean {
+  type: "literalBoolean";
+  value: boolean;
+}
+
+interface Add {
+  type: "add";
+  left: Expression;
+  right: Expression;
+}
+
+interface Equals {
+  type: "equals";
+  left: Expression;
+  right: Expression;
+}
+
+interface VariableExpression {
+  type: "variable";
+  variable: string;
+}
+
+interface Callback {
+  type: "callback";
+  line: number;
+}
+
+type Expression =
+  | LiteralString
+  | LiteralInteger
+  | LiteralBoolean
+  | Add
+  | Equals
+  | Callback;
+
+interface Assign {
+  type: "assign";
+  variable: string;
+  value: Expression;
+}
+
+interface Print {
+  type: "print";
+  value: Expression;
+}
+
+interface Clear {
+  type: "clear";
+}
+
+interface Open {
+  type: "open";
+  tag: string;
+}
+
+interface Close {
+  type: "close";
+}
+
+interface Attribute {
+  type: "attribute";
+  value: Expression;
+}
+
+interface End {
+  type: "end";
+}
+
+interface Goto {
+  type: "goto";
+  statement: number;
+}
+
+type Statement = Assign | Print | Clear | Open | Close | Attribute | End | Goto;
+
+interface Program {
+  statements: Statement[];
+}
